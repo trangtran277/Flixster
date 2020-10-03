d6568eddb1e39a41368c167027d42da4c7fdcfdd
trangtran277 <trangtran.1997@gmail.com>
2020-10-02 21:51:59 -0500

    add trailer to DetailActivity

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -11,6 +11,8 @@ import java.util.List;
 @Parcel
 public class Movie {
 
+    int movieId;
+
     String posterPath;
     String title;
     String overView;
@@ -24,6 +26,7 @@ public class Movie {
         title = jsonObject.getString("title");
         overView = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -48,5 +51,9 @@ public class Movie {
 
     public double getRating() {
         return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
